7b63906a476a41e27a882b631ce55cb7c9e5d8ba
ramakira <akirahadian@gmail.com>
2019-09-03 21:37:28 +0700

    Tugas Java OOP Concept Tutorial

---
+++ praxisday2/src/Library/Perpustakaan.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Library;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Akira
+ */
+public class Perpustakaan {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        
+        //var temporary
+      //  int jenisBuku;
+        int tempId =0;
+        String tempName = "";
+        String tempGrade = "";
+        
+        int tempKodeBuku = 0;
+        String tempNamaBuku = "";
+        String tempJenisBuku;
+        int tempKodeTransaksi = 0;
+        int tempPengembalianBuku = 0;
+        
+        System.out.println("======================================================");
+        System.out.println("               Input Cetak Bukti                   ");
+        System.out.println("======================================================");
+        
+        System.out.print("Id Pelanggan  : ");
+        tempId = input.nextInt();
+        
+        tempName = input.nextLine();
+        System.out.print("Nama Siswa : ");
+        tempName = input.nextLine();
+        
+        System.out.print("Grade Siswa : ");
+        tempGrade = input.nextLine();
+        
+        System.out.print("\nKode Buku : ");
+        tempKodeBuku = input.nextInt();
+        
+        tempNamaBuku = input.nextLine();
+        System.out.print("Nama Buku : ");
+        tempNamaBuku = input.nextLine();
+        
+        System.out.print("Jenis Buku : ");
+        tempJenisBuku = input.nextLine();
+        
+        System.out.print("Kode Transaksi : ");
+        tempKodeTransaksi = input.nextInt();
+        
+//        System.out.println("\n Pengembalian Buku : ");
+//        tempPengembalianBuku = input.nextInt();
+        
+        //eksekusi
+        Transaction t = new Transaction(tempId, tempName, tempGrade);
+        t.setBook(tempKodeBuku, tempNamaBuku, tempJenisBuku);
+        t.setpengembalianBuku (tempKodeTransaksi);
+        
+        t.cetakBuktiPeminjaman();
+    }
+}
---
+++ praxisday2/src/Library/Student.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Library;
+
+/**
+ *
+ * @author Akira
+ */
+public class Student {
+    
+    private int id;
+    private String name;
+    private String grade;
+    
+    //konstruktor
+    public Student(){
+    }
+    
+    public Student(int id, String name, String grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        
+    }
+    
+    //proses get set
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void SetName(String name){
+        this.name = name;
+    }
+    
+    public String getGrade(){
+        return grade;
+    }
+    
+    public void setName(String grade){
+        this.grade = grade;
+    }
+}
